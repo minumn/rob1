724c76d92cd45263a9e0cb4d2a33a14540caf9f6
Bens93 <Ben93@live.dk>
2018-12-10 14:25:52 +0100

    Torque data output

--- au_crustcrawler_base/nodes/Torque_Data.py
+++ au_crustcrawler_base/nodes/Torque_Data.py
@@ -3,6 +3,19 @@
 import rospy
 from std_msgs.msg import Float64
 
-from dynamixel_msgs import JointState
+from dynamixel_msgs.msg import *
 
-asdasd
+class torque_listener:
+	
+	def callback(self, data):
+		print data.load
+
+	def __init__(self):
+		print "subscribed :-)"
+		self.sub = rospy.Subscriber("joint2/state", JointState, self.callback)
+
+
+if __name__ == "__main__":
+	rospy.init_node("Torque_Data")
+	node = torque_listener()
+	rospy.spin()
